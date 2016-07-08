7949bb75d4029a7136972ed33aee2b27025113d8
Kim Mok <cafe.mui@gmail.com>
2016-07-08 12:00:21 -0400

    add a new attribute to button, implemented submitOrder(), display() when button press the value changes to 1

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.justjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /*
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    /*
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number){
+        TextView valueTextView = (TextView) findViewById(
+                R.id.value_text_view);
+        valueTextView.setText(""+ number);
     }
 }
