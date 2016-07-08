f67bcbbb09711cb1ffdc35190621a979fe997309
Kim Mok <cafe.mui@gmail.com>
2016-07-08 12:27:26 -0400

    add displayPrice() and 2 textviews

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +19,23 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view){
-        display(1);
+        display(5);
+        displayPrice(2 * 5);
     }
 
     /*
      * This method displays the given quantity value on the screen.
      */
     private void display(int number){
-        TextView valueTextView = (TextView) findViewById(
-                R.id.value_text_view);
+        TextView valueTextView = (TextView) findViewById(R.id.value_text_view);
         valueTextView.setText(""+ number);
+    }
+
+    /*
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.pricevalue_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
