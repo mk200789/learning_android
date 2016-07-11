378ab131af86f5d975926bf757dd6588332592d4
Kim Mok <cafe.mui@gmail.com>
2016-07-11 13:04:48 -0400

    implmented displayForTeamB(), addTwoPointsTeamB(), addThreePointsTeamB(), AddFreePointsTeamB()

--- CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,13 +7,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score = 0;
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(score);
     }
 
     /*
@@ -21,22 +21,50 @@ public class MainActivity extends AppCompatActivity {
      */
     public void displayForTeamA(int score){
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        scoreView.setText(String.valueOf(scoreTeamA));
     }
 
-    public void addTwoPoints(View view){
-        score = score + 2;
-        displayForTeamA(score);
+
+    /*
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(scoreTeamB));
     }
 
-    public void addThreePoints(View view){
-        score = score + 3;
-        displayForTeamA(score);
+
+
+    public void addTwoPointsTeamA(View view){
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
-    public void AddFreePoints(View view){
-        score = score + 1;
-        displayForTeamA(score);
+    public void addThreePointsTeamA(View view){
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void AddFreePointsTeamA(View view){
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+
+
+    public void addTwoPointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addThreePointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void AddFreePointsTeamB(View view){
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
 
 }
