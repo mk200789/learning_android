3f57e82decf2e0b5ee4fd7d3998e1383511308d7
Kim Mok <cafe.mui@gmail.com>
2016-07-12 14:56:03 -0400

    addedd two images, and implemented eatCookie()

--- Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view){
+        ImageView eatingImage = (ImageView) findViewById(R.id.cookie_img_view);
+        eatingImage.setImageResource(R.drawable.after_cookie);
+
+        TextView eatingText = (TextView) findViewById(R.id.stment_text_view);
+        eatingText.setText("I'm so full");
+    }
+
 }
